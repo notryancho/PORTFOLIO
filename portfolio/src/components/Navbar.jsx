1035601fbb8d650237a1ce69d27a24ccb6febe57
notryancho <ryancho1226@hotmail.com>
2023-11-23 21:32:25 -0500

    menu selection

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import {FaBars, FaTimes} from 'react-icons/fa'
+import Logo from '../assets/logo.png'
 
 const Navbar = () => {
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f]'> 
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+        <div>
+            <img src={Logo} alt="Logo Image" style={{width: '50px'}}/>
+        </div>
 
+        {/* menu */}
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Skills</li>
+                <li>Work</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+
+        {/* Hamburgers */}
+        <div>
+            <FaBars />
+        </div>
+
+        {/* Mobile Menu */}
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Skills</li>
+            <li>Work</li>
+            <li>Contact</li>
+        </ul>
+
+        {/* Social icons */}
+        <div></div>
     </div>
   )
 }
