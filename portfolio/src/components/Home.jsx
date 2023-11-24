182fcfd2e69c22710ac65d1285ab9eb983bdda15
notryancho <ryancho1226@hotmail.com>
2023-11-23 23:56:13 -0500

    adding text to home page

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -3,8 +3,16 @@ import {HiArrowNarrowRight} from 'react-icons/hi'
 
 const Home = () => {
   return (
-    <div className='w-full h-screen bg-[#0a192f]'>
+    <div name='home' className='w-full h-screen bg-[#0a192f]'>
 
+    {/*Container */}
+    <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+        <p>Hi, my name is</p>
+        <h1>Ryan Cho</h1>
+        <h2>I'm a Full Stack Developer.</h2>
+        <p> I'm a full-stack developer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I'm focused on building responsive full-stack web applications.</p>
+
+    </div>
     </div>
   )
 }
