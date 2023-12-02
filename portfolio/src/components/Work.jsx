92463408778691059b4a7074cd1ff19c6a26fc04
notryancho <ryancho1226@hotmail.com>
2023-12-02 01:29:05 -0500

    tailwind edit to div

--- portfolio/src/components/Work.jsx
+++ portfolio/src/components/Work.jsx
@@ -3,14 +3,15 @@ import React from 'react'
 const Work = () => {
   return (
     <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
-        <div>
-            <div>
-                <p>Work</p>
-                <p>// Check out some of my recent work</p>
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>Work</p>
+                <p className='py-6'>// Check out some of my recent work</p>
             </div>
 
-            <div>
-                <div>
+            <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+                <div className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center mx-auto content-div'>
+                    {/*Hover Effects */}
                     <div>
                         <span>
 
