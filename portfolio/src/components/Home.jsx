988b4362dff6a5323bfd46ab3e3e72aa48b860c8
notryancho <ryancho1226@hotmail.com>
2023-12-01 23:11:27 -0500

    adding view work button

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -7,14 +7,17 @@ const Home = () => {
 
     {/*Container */}
     <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-        <p>Hi, my name is</p>
-        <h1>Ryan Cho</h1>
+        <p className='text-pink-600'>Hi, my name is</p>
+        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Ryan Cho</h1>
         <h2>I'm a Full Stack Developer.</h2>
         <p> I'm a full-stack developer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I'm focused on building responsive full-stack web applications.</p>
-
+        <div>
+            <button>View Work <HiArrowNarrowRight /></button>
+        </div>
     </div>
     </div>
   )
 }
 
-export default Home
+
+export default Home 
