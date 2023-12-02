7b339b7b1d8749ea3b2278818731f2918dd043cd
notryancho <ryancho1226@hotmail.com>
2023-12-02 00:45:38 -0500

    Experience title

--- portfolio/src/components/Skills.jsx
+++ portfolio/src/components/Skills.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
-import HTML from '../assets/html.png';
-import CSS from '../assets/css.png';
-import JavaScript from '../assets/javascript.png';
-import ReactImg from '../assets/react.png';
-import Node from '../assets/node.png';
-import GitHub from '../assets/github.png';
-import TailWind from '../assets/tailwind.png';
-import Mongo from '../assets/mongo.png';
-import PostgreSQL from '../assets/postgresql';
+import React from "react";
+import HTML from "../assets/html.png";
+import CSS from "../assets/css.png";
+import JavaScript from "../assets/javascript.png";
+import ReactImg from "../assets/react.png";
+import Node from "../assets/node.png";
+import GitHub from "../assets/github.png";
+import TailWind from "../assets/tailwind.png";
+import Mongo from "../assets/mongo.png";
+import PostgreSQL from "../assets/postgresql";
 
 const Skills = () => {
   return (
-    <div>Skills</div>
-  )
-}
+    <div name="skills">
+      {/* Container */}
+      <div>
+        <div>
+          <p>Experience</p>
+          <p>// These are the technologies I've worked with</p>
+        </div>
 
-export default Skills
+          <div>
+            <div>
+              <img src={HTML} alt="HTML icon" />
+            </div>
+          </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
