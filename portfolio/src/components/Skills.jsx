7833e815a9057dc31334e5e2dfe00e99c553ee09
notryancho <ryancho1226@hotmail.com>
2023-12-02 00:38:48 -0500

    import images

--- portfolio/src/components/Skills.jsx
+++ portfolio/src/components/Skills.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import HTML from '../assets/html.png';
+import CSS from '../assets/css.png';
+import JavaScript from '../assets/javascript.png';
+import ReactImg from '../assets/react.png';
+import Node from '../assets/node.png';
+import GitHub from '../assets/github.png';
+import TailWind from '../assets/tailwind.png';
+import Mongo from '../assets/mongo.png';
+import PostgreSQL from '../assets/postgresql';
 
 const Skills = () => {
   return (
-    <div>s</div>
+    <div>Skills</div>
   )
 }
 
