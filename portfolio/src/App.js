402ad65fc626cd3be9ee361142b46161afd8f9df
notryancho <ryancho1226@hotmail.com>
2023-12-01 23:42:39 -0500

    about page

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,14 @@
-import Navbar from './components/Navbar'
-import Home from './components/Home'
+import About from './components/About';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+
 
 function App() {
   return (
     <div>
       <Navbar />
       <Home />
+      <About />
     </div>
   );
 }
