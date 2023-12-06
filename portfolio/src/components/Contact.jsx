7d13f28129f18cf030629be387f7d7914f7acab7
notryancho <ryancho1226@hotmail.com>
2023-12-06 14:03:06 -0500

    adding form to contact page

--- portfolio/src/components/Contact.jsx
+++ portfolio/src/components/Contact.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div className='w-full h-screen bg-[#0a192f]'>
-
+    <div name="contact" className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
+        <form action="" className='flex flex-col max-w-[600px] w-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-100'>Contact</p>
+                <p className='text-gray-300 py-4'>// Submit the form below or shoot me an email - ryancho91939@gmail.com</p>
+            </div>
+            <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
+            <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
+            <textarea className='bg-[#ccd6f6]' name="message" rows="10" placeholder='Message'></textarea>
+        </form>
     </div>
   )
 }
