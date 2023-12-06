a2e563a8c63b5cfd63e56b4f718a291622a4c8dd
notryancho <ryancho1226@hotmail.com>
2023-12-06 14:21:00 -0500

    edit to form

--- portfolio/src/components/Contact.jsx
+++ portfolio/src/components/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
   return (
     <div name="contact" className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
-        <form action="" className='flex flex-col max-w-[600px] w-full'>
+        <form method='POST' action="https://getform.io/f/9556f922-8468-4470-b246-6479ddf12f17" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-100'>Contact</p>
                 <p className='text-gray-300 py-4'>// Submit the form below or shoot me an email - ryancho91939@gmail.com</p>
@@ -11,6 +11,7 @@ const Contact = () => {
             <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
             <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
             <textarea className='bg-[#ccd6f6]' name="message" rows="10" placeholder='Message'></textarea>
+            <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button>
         </form>
     </div>
   )
