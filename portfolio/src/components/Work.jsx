bc0c30a75e906b2e093bd6c337a8336b9ee8dee2
notryancho <ryancho1226@hotmail.com>
2023-12-05 19:14:00 -0500

    updates to work page

--- portfolio/src/components/Work.jsx
+++ portfolio/src/components/Work.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import brokeMan from '../assets/brokeman.png';
+import codeUp from '../assets/codeup.png';
+import hairLink from '../assets/hairlink.png';
+import merniVal from '../assets/mernival.png';
 
 const Work = () => {
   return (
@@ -9,19 +13,20 @@ const Work = () => {
                 <p className='py-6'>// Check out some of my recent work</p>
             </div>
 
-            <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+            <div style={{backgroundImage: `url(${brokeMan})`}}
+            className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
                 <div className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center mx-auto content-div'>
                     {/*Hover Effects */}
-                    <div>
-                        <span>
-
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-black tracking-wider'>
+React JS Application
                         </span>
-                        <div>
+                        <div className='pt-8 text-center'>
                             <a href="/">
-                                <button></button>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-[#0a192f] text-white-700 font-bold text-lg'>Demo</button>
                             </a>
                             <a href="/">
-                                <button></button>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-[#0a192f] text-white-700 font-bold text-lg'>Code</button>
                             </a>
                         </div>
                     </div>
